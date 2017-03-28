d6c53d4c09168e35b81efc7e610898d9060d19db
gp2015 <georgec.perez@gmail.com>
2017-03-28 15:26:40 -0400

    05_book_titles WIP

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,13 @@
 class Book
 # write your code here
+  attr_reader :title
+
+  def title=(new_title)
+    @title = new_title.capitalize!
+    return @title
+  end
 end
+
+@book = Book.new
+@book.title = "inferno hole"
+puts @book.title

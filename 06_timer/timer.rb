eb0d29cab08300a326d9772fa68fab04b34804f2
gp2015 <georgec.perez@gmail.com>
2017-03-31 15:44:07 -0400

    3 passes in timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,43 @@
+require 'date'
+
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds=(new_seconds)
+    @seconds = new_seconds
+  end
+
+  def time_string
+    
+    length = self.seconds.to_s.size
+
+    if length < 6
+      # need + 1 to make length and leading_zeros to work?
+      leading_zeros = (6 - length) + 1 
+    end
+
+    time = "0%0#{leading_zeros}d" % self.seconds
+
+    first_set = "#{time[-6]}" + "#{time[-5]}"
+    second_set = "#{time[-4]}" + "#{time[-3]}"
+    third_set = "#{time[-2]}" + "#{time[-1]}"    
+
+    if (third_set.to_i / 60) > 0
+      second_set = "0%01d" % (second_set.to_i + (third_set.to_i / 60)).to_s
+      third_set = "0%01d" % (third_set.to_i - (second_set.to_i * 60)).to_s
+    end
+
+    format = "#{first_set}:#{second_set}:#{third_set}"
+    return format
+  end
+
 end
+
+timer = Timer.new
+timer.seconds = 60
+puts timer.time_string
